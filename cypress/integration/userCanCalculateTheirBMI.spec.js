90bccbe468f4b4c731be3c2546517d659df9b396
psnoya <paulina.s.noya@gmail.com>
2021-02-04 23:07:19 +0100

    removing app.test.js

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -1,12 +1,24 @@
 /// <reference types="Cypress" /> 
 
 describe('BMI Calculator', () => {
-    it('Calculates BMI in metric',() => {
+    beforeEach(() => {
         cy.visit('/');
+    })
+
+    it('Calculates BMI in metric',() => {
+        cy.get('select#select-method').select('metric');
         cy.get('input#weight').type(90);
         cy.get('input#height').type(190);
         cy.get('button#calculate').click();
-        cy.get('p#bmi-message').should('contain', 'You should Take a shot')
-    })
-})
+        cy.get('p#bmi-message').should('contain', 'You should Take a shot');
+    });
+
+    it('Calculates BMI in imperial',() => {
+        cy.get('select#select-method').select('imperial');
+        cy.get('input#weight').type(198);
+        cy.get('input#height').type(74);
+        cy.get('button#calculate').click();
+        cy.get('p#bmi-message').should('contain', 'You are Overweight with BMI of 25.42');
+    });
+});
 
